8bbe49a563d37e93a5798bd1604ed6b3bca43db1
rortegaes <rortega@expertsystem.com>
2018-08-10 14:13:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-#from .data_loading import data_loading
-from .experiments import experiments
-from .models import models
+from .data_loading import data_loading.py
+from .experiments import experiments.py
+from .models import models.py
